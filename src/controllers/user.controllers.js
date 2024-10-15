2e7a0deecc6f53c88700e5814e004868370fa6ca
redeagle17 <ankursingh1438@gmail.com>
2024-10-16 01:25:03 +0530

    FEAT: user signup added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.models.js";
 import responseHandler from "../utils/responseHandler.js";
+import { hash } from "bcrypt";
 
 const getAllUsers = async (req, res, next) => {
 
@@ -14,4 +15,50 @@ const getAllUsers = async (req, res, next) => {
     }
 };
 
-export { getAllUsers };
+const userSignup = async (req, res, next) => {
+    try {
+
+        const { name, email, password, confirmPassword } = req.body;
+        if (!name || !email || !password || !confirmPassword) {
+            return res.status(400).json(responseHandler(400, "All fields are required"));
+        }
+    
+        const normalizedEmail = email.toLowerCase();
+        const user = await User.findOne({ email: normalizedEmail });
+    
+        if (user) {
+            return res.status(409).json(responseHandler(409, "User with this email already exists"));
+        }
+    
+        if (password !== confirmPassword) {
+            return res.status(400).json(responseHandler(400, "Password does not match"));
+        }
+    
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json(responseHandler(400, "Invalid email format"));
+        }
+    
+        if (password.length < 6) {
+            return res.status(400).json(responseHandler(400, "Password must be at least 6 characters long"));
+        }
+    
+        const hashedPassword = await hash(password, 10);
+        const newUser = new User({
+            name: name,
+            email: normalizedEmail,
+            password: hashedPassword,
+        });
+    
+        await newUser.save();
+
+        const createdUser = { user_id: newUser.user_id, name: newUser.name, email: newUser.email };
+
+        res.status(201).json(responseHandler(201, "User registered successfully", createdUser));
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export { getAllUsers, userSignup };
